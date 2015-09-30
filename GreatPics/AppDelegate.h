868a86e58ea2d6ae352df6f66f10d8ffbc535c4f
Kateryna Zaikina <kateryna.zaikina@gmail.com>
2015-09-30 20:05:35 +0300

    Initial Commit

---
+++ GreatPics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GreatPics
+//
+//  Created by kateryna.zaikina on 9/30/15.
+//  Copyright © 2015 kateryna.zaikina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GreatPics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GreatPics
+//
+//  Created by kateryna.zaikina on 9/30/15.
+//  Copyright © 2015 kateryna.zaikina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
